21d058464386b693c41e8172dde83905a49eb437
Ikechi Michael <mykehell123@gmail.com>
2017-07-08 23:30:08 +0100

    added BooksController

---
+++ GraphQLWebAPI/Controllers/BooksController.cs
@@ -0,0 +1,27 @@
+﻿using GraphQLWebAPI.Models;
+using GraphQLWebAPI.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace GraphQLWebAPI.Controllers
+{
+    public class BooksController: ApiController
+    {
+        private readonly BookRepository bookRepository = new BookRepository();
+
+        // GET api/values
+        public IEnumerable<BookModel> Get()
+        {
+            return bookRepository.AllBooks();
+        }
+
+        // GET api/values/5
+        public BookModel Get(string isbn)
+        {
+            return bookRepository.BookByIsbn(isbn);
+        }
+    }
+}
