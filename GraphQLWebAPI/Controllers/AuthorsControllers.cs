9963c05ad32aba6a2ee230520cec7297bbf50891
Ikechi Michael <mykehell123@gmail.com>
2017-07-08 23:37:34 +0100

    added AuthorsController

---
+++ GraphQLWebAPI/Controllers/AuthorsControllers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using GraphQLWebAPI.Models;
+using GraphQLWebAPI.Repositories;
+
+namespace GraphQLWebAPI.Controllers
+{
+    public class AuthorsControllers: ApiController
+    {
+        private readonly BookRepository bookRepository = new BookRepository();
+        // GET api/authors
+        public IEnumerable<AuthorModel> Get()
+        {
+            return bookRepository.AllAuthors();
+        }
+
+        // GET api/authors/5
+        public AuthorModel Get(int id)
+        {
+            return bookRepository.AuthorById(id);
+        }
+
+        // GET api/authors/book/5
+        public AuthorModel Book(string isbn)
+        {
+            return bookRepository.BookByIsbn(isbn).Author;
+        }
+    }
+}
