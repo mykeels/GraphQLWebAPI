06da100d3375bb2c445a091df35f32ca7e69e0a2
Ikechi Michael <mykehell123@gmail.com>
2017-07-08 23:29:52 +0100

    added concrete class for IBookRepository

---
+++ GraphQLWebAPI/Repositories/BookRepository.cs
@@ -0,0 +1,52 @@
+﻿using GraphQLWebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GraphQLWebAPI.Repositories
+{
+    public class BookRepository : IBookRepository
+    {
+        private IEnumerable<BookModel> _books = new List<BookModel>();
+        private IEnumerable<AuthorModel> _authors = new List<AuthorModel>();
+        private IEnumerable<PublisherModel> _publisher = new List<PublisherModel>();
+
+        public BookRepository()
+        {
+            _books = BookModel.GetBooks(10).ToList();
+            _authors = _books.Select(book => book.Author).ToList();
+            _publisher = _books.Select(book => book.Publisher).ToList();
+        }
+
+        public IEnumerable<AuthorModel> AllAuthors()
+        {
+            return _authors;
+        }
+
+        public IEnumerable<BookModel> AllBooks()
+        {
+            return _books;
+        }
+
+        public IEnumerable<PublisherModel> AllPublishers()
+        {
+            return _publisher;
+        }
+
+        public AuthorModel AuthorById(int id)
+        {
+            return _authors.First(_ => _.Id == id);
+        }
+
+        public BookModel BookByIsbn(string isbn)
+        {
+            return _books.First(_ => _.Isbn == isbn);
+        }
+
+        public PublisherModel PublisherById(int id)
+        {
+            return _publisher.First(_ => _.Id == id);
+        }
+    }
+}
