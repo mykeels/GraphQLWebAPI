4a1c079056c253f2da3a3e61a83be4cc83d4344b
Ikechi Michael <mykehell123@gmail.com>
2017-07-08 23:28:57 +0100

    added PublisherModel

---
+++ GraphQLWebAPI/Models/PublisherModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GraphQLWebAPI.Models
+{
+    public class PublisherModel
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+        public BookModel[] Books { get; set; } = new BookModel[] { };
+
+        public AuthorModel[] Authors { get; set; } = new AuthorModel[] { };
+    }
+}
