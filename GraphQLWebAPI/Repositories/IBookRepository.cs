5b8d1cf1aab872124bdcc7d45901e2991d7a3e46
Ikechi Michael <mykehell123@gmail.com>
2017-07-08 23:29:31 +0100

    added Book Repository Interface

---
+++ GraphQLWebAPI/Repositories/IBookRepository.cs
@@ -0,0 +1,20 @@
+﻿using GraphQLWebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GraphQLWebAPI.Repositories
+{
+    public interface IBookRepository
+    {
+        BookModel BookByIsbn(string isbn);
+        IEnumerable<BookModel> AllBooks();
+
+        AuthorModel AuthorById(int id);
+        IEnumerable<AuthorModel> AllAuthors();
+
+        PublisherModel PublisherById(int id);
+        IEnumerable<PublisherModel> AllPublishers();
+    }
+}
