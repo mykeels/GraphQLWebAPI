15968176d9210619ca1aa9f0094494a5b7f76070
Ikechi Michael <mykehell123@gmail.com>
2017-07-08 23:29:06 +0100

    added AuthorModel

---
+++ GraphQLWebAPI/Models/AuthorModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace GraphQLWebAPI.Models
+{
+    public class AuthorModel
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public DateTime Birthdate { get; set; }
+
+        public BookModel[] Books { get; set; } = new BookModel[] { };
+    }
+}
