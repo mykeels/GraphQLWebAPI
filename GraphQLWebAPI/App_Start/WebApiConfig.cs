41f75448e1c5947bfdc3d36b0db19bd8f084983d
Ikechi Michael <mykehell123@gmail.com>
2017-07-08 23:30:35 +0100

    added action to route url structure for Web API

--- GraphQLWebAPI/App_Start/WebApiConfig.cs
+++ GraphQLWebAPI/App_Start/WebApiConfig.cs
@@ -16,8 +16,8 @@ namespace GraphQLWebAPI
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
+                routeTemplate: "api/{controller}/{action}/{id}",
+                defaults: new { action = RouteParameter.Optional, id = RouteParameter.Optional }
             );
         }
     }
