8fee63d50f74e6b6e941420d31fd8e4d7f02281a
Ikechi Michael <mykehell123@gmail.com>
2017-07-08 23:28:23 +0100

    added Number helper class for random number generation :p

---
+++ GraphQLWebAPI/Extensions/Number.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GraphQLWebAPI.Extensions
+{
+    public class Number
+    {
+        private static int seed = DateTime.Now.Millisecond;
+        static Random rand = new Random(DateTime.Now.Millisecond);
+
+        public static double Rnd()
+        {
+            return rand.NextDouble();
+        }
+    }
+}
