3466332d243142bb1ea0d7079727a7eb907f8d7d
Ikechi Michael <mykehell123@gmail.com>
2017-07-09 01:02:18 +0100

    added GraphController

---
+++ GraphQLWebAPI/Controllers/GraphController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using GraphQL;
+using GraphQL.Types;
+using System.Threading.Tasks;
+using GraphQL.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using GraphQLWebAPI.Queries;
+
+namespace GraphQLWebAPI.Controllers
+{
+    public class GraphController: ApiController
+    {
+        [HttpPost]
+        [Route("~/api/graph")]
+        public async Task<IHttpActionResult> Post([FromBody] string query)
+        {
+            var result = await new DocumentExecuter().ExecuteAsync((options) =>
+            {
+                options.Schema = new Schema() { Query = new BooksQuery() };
+                options.Query = query;
+            });
+            CheckForErrors(result);
+            var json = new DocumentWriter(indent: true).Write(result);
+
+            return Ok(JObject.Parse(json));
+        }
+
+        private void CheckForErrors(ExecutionResult result)
+        {
+            if (result.Errors?.Count > 0)
+            {
+                var errors = new List<Exception>();
+                foreach (var error in result.Errors)
+                {
+                    var ex = new Exception(error.Message);
+                    if (error.InnerException != null)
+                    {
+                        ex = new Exception(error.Message, error.InnerException);
+                    }
+                    errors.Add(ex);
+                }
+                throw new AggregateException(errors);
+            }
+        }
+    }
+}
